3700ffd92bf284daf5167b1e34b236e5b6cb3729
Guevana <2100929226@qq.com>
2026-05-26 11:58:24 +0800

    feat: add weapon spawning and switching

--- Source/ActionDemo/Components/Equipment/ADWeaponManagerComponent.cpp
+++ Source/ActionDemo/Components/Equipment/ADWeaponManagerComponent.cpp
@@ -2,6 +2,7 @@
 
 #include "Character/Base/ADCharacterBase.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
 #include "Equipment/ADWeaponBase.h"
 
 UADWeaponManagerComponent::UADWeaponManagerComponent()
@@ -13,12 +14,47 @@ void UADWeaponManagerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	BindWeaponOwner(EquippedWeapon);
+	if (IsValid(EquippedWeapon))
+	{
+		FADWeaponSpawnConfig ExistingWeaponConfig;
+		RegisterWeaponInstance(EquippedWeapon, ExistingWeaponConfig, false);
+		SetEquippedWeapon(EquippedWeapon);
+	}
+
+	if (bSpawnWeaponsOnBeginPlay)
+	{
+		SpawnConfiguredWeapons();
+	}
+
+	if (!HasEquippedWeapon() && !WeaponInstances.IsEmpty())
+	{
+		if (!EquipWeaponByIndex(InitialWeaponIndex))
+		{
+			EquipWeaponByDirection(1);
+		}
+	}
+
+	RefreshWeaponActivationStates();
 }
 
 void UADWeaponManagerComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	ClearWeaponOwner(EquippedWeapon);
+	for (int32 WeaponIndex = 0; WeaponIndex < WeaponInstances.Num(); ++WeaponIndex)
+	{
+		AADWeaponBase* Weapon = WeaponInstances[WeaponIndex];
+		ClearWeaponOwner(Weapon);
+		if (bDestroySpawnedWeaponsOnEndPlay && bGeneratedWeaponInstances.IsValidIndex(WeaponIndex) && bGeneratedWeaponInstances[WeaponIndex] && IsValid(Weapon))
+		{
+			Weapon->Destroy();
+		}
+	}
+
+	WeaponInstances.Reset();
+	WeaponInstanceConfigs.Reset();
+	bGeneratedWeaponInstances.Reset();
+	EquippedWeapon = nullptr;
+	EquippedWeaponIndex = INDEX_NONE;
+	bConfiguredWeaponsSpawned = false;
 
 	Super::EndPlay(EndPlayReason);
 }
@@ -43,19 +79,126 @@ bool UADWeaponManagerComponent::HasEquippedWeapon() const
 	return IsValid(EquippedWeapon);
 }
 
+int32 UADWeaponManagerComponent::GetEquippedWeaponIndex() const
+{
+	return EquippedWeaponIndex;
+}
+
+int32 UADWeaponManagerComponent::GetWeaponInstanceCount() const
+{
+	return WeaponInstances.Num();
+}
+
+AADWeaponBase* UADWeaponManagerComponent::GetWeaponInstanceAtIndex(int32 WeaponIndex) const
+{
+	return WeaponInstances.IsValidIndex(WeaponIndex) ? WeaponInstances[WeaponIndex] : nullptr;
+}
+
+bool UADWeaponManagerComponent::SpawnConfiguredWeapons()
+{
+	if (bConfiguredWeaponsSpawned)
+	{
+		return false;
+	}
+
+	bConfiguredWeaponsSpawned = true;
+	bool bSpawnedAnyWeapon = false;
+
+	for (const FADWeaponSpawnConfig& WeaponConfig : WeaponSpawnConfigs)
+	{
+		if (SpawnWeaponFromConfig(WeaponConfig) != nullptr)
+		{
+			bSpawnedAnyWeapon = true;
+		}
+	}
+
+	if (!HasEquippedWeapon() && bSpawnedAnyWeapon)
+	{
+		if (!EquipWeaponByIndex(InitialWeaponIndex))
+		{
+			EquipWeaponByDirection(1);
+		}
+	}
+
+	return bSpawnedAnyWeapon;
+}
+
+AADWeaponBase* UADWeaponManagerComponent::SpawnWeaponFromConfig(const FADWeaponSpawnConfig& WeaponConfig)
+{
+	AADCharacterBase* OwningCharacter = GetOwningCharacter();
+	UWorld* World = GetWorld();
+	if (OwningCharacter == nullptr || World == nullptr)
+	{
+		return nullptr;
+	}
+
+	if (WeaponConfig.WeaponClass == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ActionDemo] Weapon spawn skipped: WeaponClass is null. Owner=%s"), *GetNameSafe(OwningCharacter));
+		return nullptr;
+	}
+
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.Owner = OwningCharacter;
+	SpawnParameters.Instigator = OwningCharacter;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	AADWeaponBase* SpawnedWeapon = World->SpawnActor<AADWeaponBase>(
+		WeaponConfig.WeaponClass,
+		OwningCharacter->GetActorTransform(),
+		SpawnParameters);
+
+	if (!IsValid(SpawnedWeapon))
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("[ActionDemo] Weapon spawn failed. Owner=%s WeaponClass=%s"),
+			*GetNameSafe(OwningCharacter),
+			*GetNameSafe(WeaponConfig.WeaponClass.Get()));
+		return nullptr;
+	}
+
+	RegisterWeaponInstance(SpawnedWeapon, WeaponConfig, true);
+	AttachWeaponToCharacter(SpawnedWeapon, WeaponConfig);
+	SetWeaponActive(SpawnedWeapon, false);
+
+	return SpawnedWeapon;
+}
+
 void UADWeaponManagerComponent::SetEquippedWeapon(AADWeaponBase* InWeapon)
 {
 	if (EquippedWeapon == InWeapon)
 	{
 		BindWeaponOwner(EquippedWeapon);
+		if (IsValid(EquippedWeapon))
+		{
+			const int32 WeaponIndex = FindWeaponIndex(EquippedWeapon);
+			EquippedWeaponIndex = WeaponIndex;
+			const FADWeaponSpawnConfig WeaponConfig = WeaponInstanceConfigs.IsValidIndex(WeaponIndex) ? WeaponInstanceConfigs[WeaponIndex] : FADWeaponSpawnConfig();
+			AttachWeaponToCharacter(EquippedWeapon, WeaponConfig);
+		}
+		RefreshWeaponActivationStates();
 		return;
 	}
 
 	AADWeaponBase* OldWeapon = EquippedWeapon;
-	ClearWeaponOwner(OldWeapon);
+	if (IsValid(OldWeapon) && !IsManagedWeapon(OldWeapon))
+	{
+		ClearWeaponOwner(OldWeapon);
+	}
 
 	EquippedWeapon = InWeapon;
+	EquippedWeaponIndex = FindWeaponIndex(EquippedWeapon);
 	BindWeaponOwner(EquippedWeapon);
+	if (IsValid(EquippedWeapon))
+	{
+		const FADWeaponSpawnConfig WeaponConfig =
+			WeaponInstanceConfigs.IsValidIndex(EquippedWeaponIndex) ? WeaponInstanceConfigs[EquippedWeaponIndex] : FADWeaponSpawnConfig();
+		AttachWeaponToCharacter(EquippedWeapon, WeaponConfig);
+	}
+
+	RefreshWeaponActivationStates();
 
 	OnEquippedWeaponChanged.Broadcast(OldWeapon, EquippedWeapon);
 }
@@ -63,6 +206,27 @@ void UADWeaponManagerComponent::SetEquippedWeapon(AADWeaponBase* InWeapon)
 void UADWeaponManagerComponent::ClearEquippedWeapon()
 {
 	SetEquippedWeapon(nullptr);
+}
+
+bool UADWeaponManagerComponent::EquipWeaponByIndex(int32 WeaponIndex)
+{
+	if (!WeaponInstances.IsValidIndex(WeaponIndex) || !IsValid(WeaponInstances[WeaponIndex]))
+	{
+		return false;
+	}
+
+	SetEquippedWeapon(WeaponInstances[WeaponIndex]);
+	return HasEquippedWeapon() && EquippedWeaponIndex == WeaponIndex;
+}
+
+bool UADWeaponManagerComponent::EquipNextWeapon()
+{
+	return EquipWeaponByDirection(1);
+}
+
+bool UADWeaponManagerComponent::EquipPreviousWeapon()
+{
+	return EquipWeaponByDirection(-1);
 }
 
 void UADWeaponManagerComponent::BindWeaponOwner(AADWeaponBase* Weapon) const
@@ -85,4 +249,140 @@ void UADWeaponManagerComponent::ClearWeaponOwner(AADWeaponBase* Weapon) const
 	}
 
 	Weapon->SetOwningCharacter(nullptr);
+}
+
+void UADWeaponManagerComponent::RegisterWeaponInstance(AADWeaponBase* Weapon, const FADWeaponSpawnConfig& WeaponConfig, bool bGeneratedByManager)
+{
+	if (!IsValid(Weapon))
+	{
+		return;
+	}
+
+	const int32 ExistingIndex = FindWeaponIndex(Weapon);
+	if (ExistingIndex != INDEX_NONE)
+	{
+		if (WeaponInstanceConfigs.IsValidIndex(ExistingIndex))
+		{
+			WeaponInstanceConfigs[ExistingIndex] = WeaponConfig;
+		}
+		if (bGeneratedWeaponInstances.IsValidIndex(ExistingIndex))
+		{
+			bGeneratedWeaponInstances[ExistingIndex] = bGeneratedWeaponInstances[ExistingIndex] || bGeneratedByManager;
+		}
+		BindWeaponOwner(Weapon);
+		return;
+	}
+
+	WeaponInstances.Add(Weapon);
+	WeaponInstanceConfigs.Add(WeaponConfig);
+	bGeneratedWeaponInstances.Add(bGeneratedByManager);
+	BindWeaponOwner(Weapon);
+}
+
+bool UADWeaponManagerComponent::IsManagedWeapon(const AADWeaponBase* Weapon) const
+{
+	return FindWeaponIndex(Weapon) != INDEX_NONE;
+}
+
+int32 UADWeaponManagerComponent::FindWeaponIndex(const AADWeaponBase* Weapon) const
+{
+	if (!IsValid(Weapon))
+	{
+		return INDEX_NONE;
+	}
+
+	for (int32 WeaponIndex = 0; WeaponIndex < WeaponInstances.Num(); ++WeaponIndex)
+	{
+		if (WeaponInstances[WeaponIndex] == Weapon)
+		{
+			return WeaponIndex;
+		}
+	}
+
+	return INDEX_NONE;
+}
+
+bool UADWeaponManagerComponent::EquipWeaponByDirection(int32 Direction)
+{
+	if (WeaponInstances.IsEmpty())
+	{
+		return false;
+	}
+
+	const int32 StepDirection = Direction >= 0 ? 1 : -1;
+	const int32 StartIndex = EquippedWeaponIndex != INDEX_NONE ? EquippedWeaponIndex : (StepDirection > 0 ? -1 : WeaponInstances.Num());
+
+	for (int32 Step = 1; Step <= WeaponInstances.Num(); ++Step)
+	{
+		const int32 CandidateIndex = (StartIndex + StepDirection * Step + WeaponInstances.Num()) % WeaponInstances.Num();
+		if (WeaponInstances.IsValidIndex(CandidateIndex) && IsValid(WeaponInstances[CandidateIndex]))
+		{
+			return EquipWeaponByIndex(CandidateIndex);
+		}
+	}
+
+	return false;
+}
+
+void UADWeaponManagerComponent::AttachWeaponToCharacter(AADWeaponBase* Weapon, const FADWeaponSpawnConfig& WeaponConfig) const
+{
+	AADCharacterBase* OwningCharacter = GetOwningCharacter();
+	if (!IsValid(Weapon) || OwningCharacter == nullptr)
+	{
+		return;
+	}
+
+	USkeletalMeshComponent* CharacterMesh = OwningCharacter->GetMesh();
+	if (CharacterMesh == nullptr)
+	{
+		Weapon->AttachToActor(OwningCharacter, FAttachmentTransformRules::KeepRelativeTransform);
+		Weapon->SetActorRelativeTransform(ResolveAttachRelativeTransform(WeaponConfig));
+		return;
+	}
+
+	Weapon->AttachToComponent(
+		CharacterMesh,
+		FAttachmentTransformRules::KeepRelativeTransform,
+		ResolveAttachSocketName(WeaponConfig));
+	Weapon->SetActorRelativeTransform(ResolveAttachRelativeTransform(WeaponConfig));
+}
+
+void UADWeaponManagerComponent::RefreshWeaponActivationStates() const
+{
+	for (AADWeaponBase* Weapon : WeaponInstances)
+	{
+		SetWeaponActive(Weapon, Weapon == EquippedWeapon);
+	}
+
+	if (IsValid(EquippedWeapon) && !IsManagedWeapon(EquippedWeapon))
+	{
+		SetWeaponActive(EquippedWeapon, true);
+	}
+}
+
+void UADWeaponManagerComponent::SetWeaponActive(AADWeaponBase* Weapon, bool bActive) const
+{
+	if (!IsValid(Weapon))
+	{
+		return;
+	}
+
+	if (bActive || bHideUnequippedWeapons)
+	{
+		Weapon->SetActorHiddenInGame(!bActive);
+	}
+
+	Weapon->SetActorEnableCollision(bActive && bEnableEquippedWeaponCollision);
+}
+
+FName UADWeaponManagerComponent::ResolveAttachSocketName(const FADWeaponSpawnConfig& WeaponConfig) const
+{
+	return WeaponConfig.AttachSocketName.IsNone() ? DefaultAttachSocketName : WeaponConfig.AttachSocketName;
+}
+
+FTransform UADWeaponManagerComponent::ResolveAttachRelativeTransform(const FADWeaponSpawnConfig& WeaponConfig) const
+{
+	return WeaponConfig.AttachRelativeTransform.Equals(FTransform::Identity)
+		? DefaultAttachRelativeTransform
+		: WeaponConfig.AttachRelativeTransform;
 }
--- Source/ActionDemo/Components/Equipment/ADWeaponManagerComponent.h
+++ Source/ActionDemo/Components/Equipment/ADWeaponManagerComponent.h
@@ -8,6 +8,21 @@ class AADCharacterBase;
 class AADWeaponBase;
 class USkeletalMeshComponent;
 
+USTRUCT(BlueprintType)
+struct FADWeaponSpawnConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	TSubclassOf<AADWeaponBase> WeaponClass = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	FName AttachSocketName = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	FTransform AttachRelativeTransform = FTransform::Identity;
+};
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FADEquippedWeaponChangedSignature, AADWeaponBase*, OldWeapon, AADWeaponBase*, NewWeapon);
 
 /**
@@ -34,11 +49,35 @@ public:
 	UFUNCTION(BlueprintPure, Category = "ActionDemo|Weapon")
 	bool HasEquippedWeapon() const;
 
+	UFUNCTION(BlueprintPure, Category = "ActionDemo|Weapon")
+	int32 GetEquippedWeaponIndex() const;
+
+	UFUNCTION(BlueprintPure, Category = "ActionDemo|Weapon")
+	int32 GetWeaponInstanceCount() const;
+
+	UFUNCTION(BlueprintPure, Category = "ActionDemo|Weapon")
+	AADWeaponBase* GetWeaponInstanceAtIndex(int32 WeaponIndex) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
+	bool SpawnConfiguredWeapons();
+
+	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
+	AADWeaponBase* SpawnWeaponFromConfig(const FADWeaponSpawnConfig& WeaponConfig);
+
 	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
 	void SetEquippedWeapon(AADWeaponBase* InWeapon);
 
 	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
 	void ClearEquippedWeapon();
+
+	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
+	bool EquipWeaponByIndex(int32 WeaponIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
+	bool EquipNextWeapon();
+
+	UFUNCTION(BlueprintCallable, Category = "ActionDemo|Weapon")
+	bool EquipPreviousWeapon();
 
 	UPROPERTY(BlueprintAssignable, Category = "ActionDemo|Weapon")
 	FADEquippedWeaponChangedSignature OnEquippedWeaponChanged;
@@ -47,10 +86,58 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	TArray<FADWeaponSpawnConfig> WeaponSpawnConfigs;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	bool bSpawnWeaponsOnBeginPlay = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon", meta = (ClampMin = "0"))
+	int32 InitialWeaponIndex = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	FName DefaultAttachSocketName = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	FTransform DefaultAttachRelativeTransform = FTransform::Identity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	bool bHideUnequippedWeapons = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	bool bEnableEquippedWeaponCollision = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	bool bDestroySpawnedWeaponsOnEndPlay = true;
+
 	/** 当前装备武器；武器插槽命中检测会从该 Actor 的 Mesh 读取插槽。 */
 	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "ActionDemo|Weapon")
 	TObjectPtr<AADWeaponBase> EquippedWeapon = nullptr;
 
+	UPROPERTY(Transient, BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	TArray<TObjectPtr<AADWeaponBase>> WeaponInstances;
+
+	UPROPERTY(Transient)
+	TArray<FADWeaponSpawnConfig> WeaponInstanceConfigs;
+
+	UPROPERTY(Transient)
+	TArray<bool> bGeneratedWeaponInstances;
+
+	UPROPERTY(BlueprintReadOnly, Category = "ActionDemo|Weapon")
+	int32 EquippedWeaponIndex = INDEX_NONE;
+
+	UPROPERTY(Transient)
+	bool bConfiguredWeaponsSpawned = false;
+
 	void BindWeaponOwner(AADWeaponBase* Weapon) const;
 	void ClearWeaponOwner(AADWeaponBase* Weapon) const;
+	void RegisterWeaponInstance(AADWeaponBase* Weapon, const FADWeaponSpawnConfig& WeaponConfig, bool bGeneratedByManager);
+	bool IsManagedWeapon(const AADWeaponBase* Weapon) const;
+	int32 FindWeaponIndex(const AADWeaponBase* Weapon) const;
+	bool EquipWeaponByDirection(int32 Direction);
+	void AttachWeaponToCharacter(AADWeaponBase* Weapon, const FADWeaponSpawnConfig& WeaponConfig) const;
+	void RefreshWeaponActivationStates() const;
+	void SetWeaponActive(AADWeaponBase* Weapon, bool bActive) const;
+	FName ResolveAttachSocketName(const FADWeaponSpawnConfig& WeaponConfig) const;
+	FTransform ResolveAttachRelativeTransform(const FADWeaponSpawnConfig& WeaponConfig) const;
 };
